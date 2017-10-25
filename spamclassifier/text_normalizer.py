561a51d4c5871811e78cb601d4cb9105c1db07e2
Es-sami Karim <karim.es-sami@swisscom.com>
2017-10-25 11:00:08 +0200

    Add a step in the normalization process to handle line returns in mails

--- spamclassifier/text_normalizer.py
+++ spamclassifier/text_normalizer.py
@@ -5,6 +5,7 @@ from stemming.porter2 import stem
 def normalize(mail):
     # ********* Normalization / Data Cleaning *********
     mail = mail.lower()
+    mail = mail.replace('\n', ' ')
     mail = re.sub('<[^<>]+>', '', mail)
     mail = re.sub('[0-9]+', 'number', mail)
     mail = re.sub('(http|https)://[^\s]*', 'httpaddr', mail)
@@ -16,7 +17,7 @@ def normalize(mail):
     if mail:
         words = re.split('[ @$/#.-:$*+=?!(){},\'">_<;%]', mail)
         for word in words:
-            if word:
+            if word and ' ' not in word:
                 word = re.sub('[^a-zA-Z0-9]', '', word)
                 word = stem(word)
                 cleaned_words.append(word)
